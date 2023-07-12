e7b2c1acec9680982603ff0a6728c5f41d2f3959
wahyuindra <wahyuindra712@gmail.com>
2023-07-12 10:18:19 +0700

    add project page

---
+++ src/pages/project.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { VDataTable } from 'vuetify/labs/VDataTable'
+import heroEmpty1 from '@images/hero/hero-empty1.png'
+
+const searchConnectedDevice = ref('')
+
+const connectedDevicesHeaders = [
+  { title: 'BROWSER', key: 'browser' },
+  { title: 'DEVICE', key: 'device' },
+  { title: 'LOCATION', key: 'location' },
+  { title: 'RECENT ACTIVITY', key: 'recentActivity' },
+]
+
+const connectedDevices = [
+  {
+    browser: 'Chrome on Windows',
+    device: 'HP Spectre 360',
+    location: 'New York, NY',
+    recentActivity: '28 Apr 2022, 18:20',
+    deviceIcon: { icon: 'bxl-windows', color: 'primary' },
+  },
+  {
+    browser: 'Chrome on iPhone',
+    device: 'iPhone 12x',
+    location: 'Los Angeles, CA',
+    recentActivity: '20 Apr 2022, 10:20',
+    deviceIcon: { icon: 'bx-mobile', color: 'error' },
+  },
+  {
+    browser: 'Chrome on Android',
+    device: 'Oneplus 9 Pro',
+    location: 'San Francisco, CA',
+    recentActivity: '16 Apr 2022, 04:20',
+    deviceIcon: { icon: 'bxl-android', color: 'success' },
+  },
+  {
+    browser: 'Chrome on MacOS',
+    device: 'Apple iMac',
+    location: 'New York, NY',
+    recentActivity: '28 Apr 2022, 18:20',
+    deviceIcon: { icon: 'bxl-apple', color: 'secondary' },
+  },
+  {
+    browser: 'Chrome on Windows',
+    device: 'HP Spectre 360',
+    location: 'Los Angeles, CA',
+    recentActivity: '20 Apr 2022, 10:20',
+    deviceIcon: { icon: 'bxl-windows', color: 'primary' },
+  },
+  {
+    browser: 'Chrome on Android',
+    device: 'Oneplus 9 Pro',
+    location: 'San Francisco, CA',
+    recentActivity: '16 Apr 2022, 04:20',
+    deviceIcon: { icon: 'bxl-android', color: 'success' },
+  },
+]
+
+const filteredConnectedDevices = computed(() => {
+  return connectedDevices.filter(dataDevices =>
+    dataDevices.browser.toLowerCase().includes(
+      searchConnectedDevice.value.toLowerCase(),
+    ),
+  )
+})
+</script>
+
+<template>
+  <div>
+    <h2>Selamat Datang</h2>
+    <p>Ingin pantau proyek IoT-mu yang mana?</p>
+    <VCard
+      v-if="filteredConnectedDevices.length > 0"
+      class="pa-6"
+    >
+      <VCardTitle class="pa-0">
+        Perangkat yang terhubung
+      </VCardTitle>
+      <p class="text-primary">
+        Jumlah : 20
+      </p>
+      <VSpacer />
+      <VTextField
+        v-model="searchConnectedDevice"
+        append-icon="mdi-magnify"
+        label="Cari"
+        single-line
+        hide-details
+        class="border-0"
+      />
+      <VDataTable
+        :headers="connectedDevicesHeaders"
+        :items="filteredConnectedDevices"
+        class="my-6 border"
+      />
+    </VCard>
+
+    <VCard
+      v-else
+      class="pa-12"
+    >
+      <VRow class="d-flex align-center">
+        <VCol
+          lg="6"
+          md="12"
+          class="d-flex justify-end"
+        >
+          <div>
+            <VImg
+              :src="heroEmpty1"
+              width="300"
+            />
+          </div>
+        </VCol>
+        <VCol
+          lg="3"
+          md="12"
+        >
+          <h2 class="text-center">
+            Ups, kamu belum <br> menghubungkan <br> perangkat apapun
+          </h2>
+        </VCol>
+      </VRow>
+    </VCard>
+
+    <VCol
+      lg="6"
+      md="12"
+      class="pa-0"
+    >
+      <VCard class="pa-6 col-6 mt-6 elevation-0">
+        <VCardTitle class="pa-0">
+          Ada Proyek Baru
+        </VCardTitle>
+        <p>Datarkan proyekmu terlebih dahulu</p>
+        <VBtn
+          prepend-icon="mdi-plus"
+          variant="text"
+        >
+          Tambah
+        </VBtn>
+      </VCard>
+    </VCol>
+  </div>
+</template>
