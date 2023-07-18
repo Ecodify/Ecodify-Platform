7baaf16c22aabf6d6cc36eb0384df9b241875f71
wahyuindra <wahyuindra712@gmail.com>
2023-07-18 23:15:51 +0700

    add edit & delete dialog component

---
+++ src/components/DeleteDialog.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const dialog = ref(false)
+</script>
+
+<template>
+  <VDialog
+    v-model="dialog"
+    persistent
+    width="auto"
+    content-class="my-custom-dialog"
+  >
+    <template #activator="{ props }">
+      <VBtn
+        v-bind="props"
+        class="ma-2"
+        variant="text"
+        icon="mdi-delete"
+        color="error"
+      />
+    </template>
+    <VCard>
+      <VCardTitle class="mt-6">
+        <div class="d-flex justify-space-between">
+          <p class="text-h5 ">
+            Hapus Proyek <span class="text-primary">My_Smart_IoT</span>
+          </p>
+        </div>
+      </VCardTitle>
+      <VCardSubtitle>
+        <p class="text-h12">
+          Apakah anda yakin akan menghapus proyek ini?
+        </p>
+      </VCardSubtitle>
+      <VCardText class="d-flex justify-end">
+        <VBtn
+          color="black"
+          class="d-block"
+          variant="text"
+          @click="dialog = false"
+        >
+          Batal
+        </VBtn>
+        <VBtn
+          color="error"
+          @click="dialog = false"
+        >
+          Hapus
+        </VBtn>
+      </VCardText>
+    </VCard>
+  </VDialog>
+</template>
+
+<style scoped>
+  .my-custom-dialog {
+    align-self: flex-end;
+  }
+</style>
---
+++ src/components/EditDialog.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const dialog = ref(false)
+</script>
+
+<template>
+  <VDialog
+    v-model="dialog"
+    persistent
+    width="auto"
+    content-class="my-custom-dialog"
+  >
+    <template #activator="{ props }">
+      <VBtn
+        v-bind="props"
+        class="ma-2"
+        variant="text"
+        icon="mdi-pencil"
+        color="warning"
+      />
+    </template>
+    <VCard>
+      <VCardTitle class="mt-6">
+        <div class="d-flex justify-space-between">
+          <span class="text-h5 ">Proyek baru</span>
+          <VBtn
+            variant="text"
+            @click="dialog = false"
+          >
+            <VIcon>mdi-close</VIcon>
+          </VBtn>
+        </div>
+      </VCardTitle>
+      <VCardSubtitle>
+        <p class="text-h12">
+          Buat projek untuk kemudahan pemantauan unit.
+        </p>
+      </VCardSubtitle>
+      <VCardText>
+        <VContainer class="pa-0">
+          <VRow class="d-block">
+            <VCol cols="12">
+              <VTextField
+                label="Nama Proyek"
+                required
+              />
+            </VCol>
+            <VCol cols="12">
+              <VTextarea
+                label="Deskripsi Proyek"
+                required
+              />
+            </VCol>
+            <VCol cols="12">
+              <VBtn
+                class="btn btn-primary d-block w-100"
+                @click="dialog = false"
+              >
+                Buat
+              </VBtn>
+            </VCol>
+          </VRow>
+        </VContainer>
+      </VCardText>
+    </VCard>
+  </VDialog>
+</template>
+
+<style scoped>
+  .my-custom-dialog {
+    align-self: flex-end;
+  }
+</style>
